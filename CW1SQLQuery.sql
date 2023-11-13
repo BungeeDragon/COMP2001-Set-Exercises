d67763d3536b149c75ddd653f9969a7f512b2f67
BungeeDragon <skillmanesther@gmail.com>
2023-11-13 14:14:19 +0000

    Implemented DEFAULT values

--- CW1SQLQuery.sql
+++ CW1SQLQuery.sql
@@ -26,6 +26,7 @@ CREATE TABLE CW1.Users
     UserNo INT NOT NULL IDENTITY(1,1),
     Username CHAR(81) NOT NULL,
     Email VARCHAR(320) NOT NULL,
+    userPassword VARCHAR(Max) NOT NULL, 
 
     CONSTRAINT pk_Users PRIMARY KEY (UserNo),
     CONSTRAINT UQ_Email UNIQUE (Email) -- Assuming Email is unique for each user
@@ -33,32 +34,30 @@ CREATE TABLE CW1.Users
 SET IDENTITY_INSERT CW1.Users ON;
 
 
-
 -- Insert into Users table
-INSERT INTO CW1.Users (UserNo, Username, Email)
-VALUES (1, 'Grace Hopper', 'grace@plymouth.ac.uk');
-INSERT INTO CW1.Users (UserNo, Username, Email)
-VALUES (2, 'Tim Berners-Lee', 'tim@plymouth.ac.uk');
-INSERT INTO CW1.Users (UserNo, Username, Email)
-VALUES (3, 'Ada Lovelace', 'ada@plymouth.ac.uk');
+INSERT INTO CW1.Users (UserNo, Username, Email, userPassword)
+VALUES (1, 'Grace Hopper', 'grace@plymouth.ac.uk', 'ISAD123!');
+INSERT INTO CW1.Users (UserNo, Username, Email, userPassword)
+VALUES (2, 'Tim Berners-Lee', 'tim@plymouth.ac.uk', 'COMP2000!');
+INSERT INTO CW1.Users (UserNo, Username, Email, userPassword)
+VALUES (3, 'Ada Lovelace', 'ada@plymouth.ac.uk', 'insecurePassword');
 
 -- Create UserData table
 CREATE TABLE CW1.UserData 
 ( 
-    Email VARCHAR(320) NOT NULL, 
-    userPassword VARCHAR(Max) NOT NULL,     
-    AboutMe VARCHAR(720),
-    MemberLocation VARCHAR(Max) NOT NULL, 
-    Units CHAR(255) NOT NULL 
+    Email VARCHAR(320) NOT NULL,   
+    AboutMe VARCHAR(720) DEFAULT NULL,
+    MemberLocation VARCHAR(Max) NOT NULL DEFAULT 'Plymouth, Devon, England', 
+    Units CHAR(255) NOT NULL DEFAULT 'Metric'
         CHECK (Units IN('Imperial', 'Metric')),     
-    ActivityTimePreference CHAR(255) NOT NULL
+    ActivityTimePreference CHAR(255) NOT NULL DEFAULT 'Pace'
         CHECK (ActivityTimePreference IN('Speed', 'Pace')),
-    userHeight INT
+    userHeight INT DEFAULT NULL
         CHECK (100 <= userHeight AND userHeight <= 299), 
-    userWeight INT,
+    userWeight INT DEFAULT NULL
         CHECK (23 <= userWeight AND userWeight <= 407), 
-    Birthday DATE,
-    MarketingLanguage CHAR(255) NOT NULL
+    Birthday DATE DEFAULT NULL,
+    MarketingLanguage CHAR(255) NOT NULL DEFAULT 'English(UK)'
         CHECK (MarketingLanguage IN('English (US)', 'English(UK)', 'Dansk (Danmark)','Deutsch (Deutschland)', 'Español (España)', 'Español (Latinoamérica)', 'Français (France)', 'Italiano (Italia)', 'Nederlands (Nederland)', 'Norsk bokmål (Norge)', 'Polski (Polska)', 'Português (Brasil)', 'Português (Portugal)', 'Svenska (Sverige)' )),  
 
     CONSTRAINT PK_UserData PRIMARY KEY (Email),
@@ -67,21 +66,22 @@ CREATE TABLE CW1.UserData
 
 
 -- Insert into UserData table
-INSERT INTO CW1.UserData(Email, userPassword, AboutMe, MemberLocation, Units, ActivityTimePreference, userHeight, userWeight, Birthday, MarketingLanguage)
-VALUES('grace@plymouth.ac.uk', 'ISAD123!', NULL , 'Plymouth, Devon, England', 'Metric', 'Pace', NULL, NULL, NULL, 'English(UK)');
-INSERT INTO CW1.UserData(Email, userPassword, AboutMe, MemberLocation, Units, ActivityTimePreference, userHeight, userWeight, Birthday, MarketingLanguage)
-VALUES('tim@plymouth.ac.uk', 'COMP2000!', NULL , 'Plymouth, Devon, England', 'Metric', 'Pace', NULL, NULL, NULL, 'English(UK)');
-INSERT INTO CW1.UserData(Email, userPassword, AboutMe, MemberLocation, Units, ActivityTimePreference, userHeight, userWeight, Birthday, MarketingLanguage)
-VALUES('ada@plymouth.ac.uk', 'insecurePassword', NULL , 'Plymouth, Devon, England', 'Metric', 'Pace', NULL, NULL, NULL, 'English(UK)');
+INSERT INTO CW1.UserData(Email)
+VALUES('grace@plymouth.ac.uk');
+INSERT INTO CW1.UserData(Email)
+VALUES('tim@plymouth.ac.uk');
+INSERT INTO CW1.UserData(Email)
+VALUES('ada@plymouth.ac.uk');
+
 -- SET IDENTITY_INSERT CW1.FavouriteActivities ON;
 
 -- Create FavouriteActivities table
 CREATE TABLE CW1.FavouriteActivities
 (
     UserNo INT NOT NULL,
-    Activities CHAR(255) NOT NULL,
+    Activities CHAR(255) NOT NULL DEFAULT 'Backpacking',
     CHECK (Activities IN('Backpacking', 'Bike Touring', 'Bird Watching','Camping', 'Cross-country Skiing', 'Fishing', 'Hiking', 'Horse Riding', 'Mountain Biking', 'OHV/Off-road Driving', 'Paddle Sports', 'Road Biking', 'Rock Climbing', 'Running', 'Scenic Driving', 'Skiing', 'Snowshoeing', 'Via Ferrata', 'Walking')),  
-    FavouriteActivities BIT NOT NULL,
+    FavouriteActivities BIT NOT NULL DEFAULT 0,
     CHECK (FavouriteActivities IN (0, 1)),
     CONSTRAINT PK_FavouriteActivities PRIMARY KEY (UserNo, Activities),
     CONSTRAINT FK_FavouriteActivities_Users FOREIGN KEY (UserNo) REFERENCES CW1.Users (UserNo)
@@ -165,3 +165,5 @@ JOIN CW1.FavouriteActivities fa ON u.UserNo = fa.UserNo
 WHERE fa.FavouriteActivities = 1;
 
 --Triggers
+
+
