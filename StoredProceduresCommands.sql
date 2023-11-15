ebac0b7d0cded8f911c293fe8373958a173a46c6
BungeeDragon <skillmanesther@gmail.com>
2023-11-15 16:17:33 +0000

    Initializing stored procedures

---
+++ StoredProceduresCommands.sql
@@ -0,0 +1,5 @@
+--EXEC CW1.InsertUser @Username = 'Alan Turing', @Email = 'alan@plymouth.ac.uk', @Password = 'Enigma123!';
+
+--EXEC CW1.UpdateUser @UserNo = 3, @newUsername = 'Esther Skillman', @newEmail = 'esther@plymouth.ac.uk', @newPassword = 'Boop2000!';
+
+--EXEC CW1.DeleteUser @UserNo = 4;
